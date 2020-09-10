7362af2afb5ed068d93c23a5c8d6b684437c46df
Brandon Islas <brandon98642@gmail.com>
2020-09-10 13:00:35 -0500

    Primearas Aplicaciones

---
+++ ProgramacionOrientadaaObjetos/Aplicacion1/Principal.py
@@ -0,0 +1,5 @@
+from MayorMenor import numero_menor
+
+n1=int(input("Ingrese el primer numero: "))
+n2=int(input("Ingrese el primer numero: "))
+print("El menor de los dos valores es: ",numero_menor(n1,n2))
---
+++ ProgramacionOrientadaaObjetos/Aplicacion2/Principal.py
@@ -0,0 +1,5 @@
+import operacionlista
+
+lista=operacionlista.cargar_datos()
+operacionlista.verificar_mayor(lista)
+operacionlista.verificar_suma(lista)
